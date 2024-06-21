ad12d390e41d26fc45337d8b1e765dffb89c06f8
si eun <tldms123455@naver.com>
2024-06-21 13:05:14 +0900

    first commit

---
+++ src/test/java/com/sini/lambda_stream_practice/LambdaStreamPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sini.lambda_stream_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaStreamPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
